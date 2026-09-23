0efb4051bc488c86eae19ee08c4ca9c6c50c7b06
Vaishnavi Gattu <vgattu@evoketechnologies.com>
2026-09-23 18:52:18 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function HandleSubmit()
+{
+    var name = document.getElementById("name").value;
+    var rollnumber = document.getElementById("rollnumber").value;
+    alert("Name: " + name + "\nRollNumber: " + rollnumber);
+}
